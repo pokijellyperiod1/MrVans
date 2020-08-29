e08b336871f7d9545c6cbdd04b0a3fbc7f2e4f4f
TanKy <pokijellyperiod1@gmail.com>
2020-08-29 13:58:42 +0700

    đá

--- src/main/java/com/tanky/MrVan/API/Mon_API.java
+++ src/main/java/com/tanky/MrVan/API/Mon_API.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -55,6 +56,7 @@ public class Mon_API {
 		
 		return ResponseEntity.ok(monService.save(entity));
 	}
+	
 	
 	
 	
